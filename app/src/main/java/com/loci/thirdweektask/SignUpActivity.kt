a314db6b64db320b716c18e23f8f7881fa5a7c6e
kdbswo <kdbswo@gmail.com>
2024-06-22 16:07:10 +0900

    Add Lv2

--- app/src/main/java/com/loci/thirdweektask/SignUpActivity.kt
+++ app/src/main/java/com/loci/thirdweektask/SignUpActivity.kt
@@ -1,6 +1,9 @@
 package com.loci.thirdweektask
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,21 @@ class SignUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val finishButton = findViewById<Button>(R.id.btn_signIn)
+        val editTextName = findViewById<EditText>(R.id.et_name)
+        val editTextId = findViewById<EditText>(R.id.et_id)
+        val editTextPassword = findViewById<EditText>(R.id.et_password)
+
+        finishButton.setOnClickListener {
+
+            if (editTextName.text.isEmpty() || editTextId.text.isEmpty() || editTextPassword.text.isEmpty()) {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                finish()
+            }
+
         }
     }
 }
