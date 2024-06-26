544aa6af823e359432530462958c46c8bb8c967f
kdbswo <kdbswo@gmail.com>
2024-06-26 17:40:41 +0900

    Update main_page background color

--- app/src/main/java/com/loci/thirdweektask/HomeActivity.kt
+++ app/src/main/java/com/loci/thirdweektask/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.loci.thirdweektask
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
@@ -15,7 +18,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_page)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -25,14 +28,30 @@ class HomeActivity : AppCompatActivity() {
         val editTextId = findViewById<TextView>(R.id.tv_homeId)
         val buttonEnd = findViewById<Button>(R.id.btn_end)
         val imageViewBall = findViewById<ImageView>(R.id.img_ball)
+        val mainPage = findViewById<ConstraintLayout>(R.id.main_page)
 
         val random = Random.nextInt(5)
         when (random) {
-            0 -> imageViewBall.setImageResource(R.drawable.baseball_image)
-            1 -> imageViewBall.setImageResource(R.drawable.pngegg)
-            2 -> imageViewBall.setImageResource(R.drawable.volleyball_remove_bg)
-            3 -> imageViewBall.setImageResource(R.drawable.rugb_removebg)
-            4 -> imageViewBall.setImageResource(R.drawable.basketball_removebg)
+            0 -> {
+                imageViewBall.setImageResource(R.drawable.baseball_image)
+                mainPage.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            }
+            1 -> {
+                imageViewBall.setImageResource(R.drawable.pngegg)
+                mainPage.setBackgroundColor(Color.parseColor("#D7FFD9"))
+            }
+            2 -> {
+                imageViewBall.setImageResource(R.drawable.volleyball_remove_bg)
+                mainPage.setBackgroundColor(Color.parseColor("#FFFDD7"))
+            }
+            3 -> {
+                imageViewBall.setImageResource(R.drawable.rugb_removebg)
+                mainPage.setBackgroundColor(Color.parseColor("#D7EEFF"))
+            }
+            4 -> {
+                imageViewBall.setImageResource(R.drawable.basketball_removebg)
+                mainPage.setBackgroundColor(Color.parseColor("#FFD7D7"))
+            }
         }
 
         editTextId.setText(idData)
