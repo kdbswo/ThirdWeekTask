d14c532356c0a7e9ce069f275d5f88abfc3073bc
kdbswo <kdbswo@gmail.com>
2024-06-27 20:02:03 +0900

    Update end button and selector in HomeActivity

--- app/src/main/java/com/loci/thirdweektask/HomeActivity.kt
+++ app/src/main/java/com/loci/thirdweektask/HomeActivity.kt
@@ -2,13 +2,11 @@ package com.loci.thirdweektask
 
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
@@ -26,7 +24,7 @@ class HomeActivity : AppCompatActivity() {
 
         val idData = intent.getStringExtra("id")
         val editTextId = findViewById<TextView>(R.id.tv_homeId)
-        val buttonEnd = findViewById<Button>(R.id.btn_end)
+        val buttonEnd = findViewById<ConstraintLayout>(R.id.btn_main_end)
         val imageViewBall = findViewById<ImageView>(R.id.img_ball)
         val mainPage = findViewById<ConstraintLayout>(R.id.main_page)
 
@@ -36,18 +34,22 @@ class HomeActivity : AppCompatActivity() {
                 imageViewBall.setImageResource(R.drawable.baseball_image)
                 mainPage.setBackgroundColor(Color.parseColor("#FFFFFF"))
             }
+
             1 -> {
                 imageViewBall.setImageResource(R.drawable.pngegg)
                 mainPage.setBackgroundColor(Color.parseColor("#D7FFD9"))
             }
+
             2 -> {
                 imageViewBall.setImageResource(R.drawable.volleyball_remove_bg)
                 mainPage.setBackgroundColor(Color.parseColor("#FFFDD7"))
             }
+
             3 -> {
                 imageViewBall.setImageResource(R.drawable.rugb_removebg)
                 mainPage.setBackgroundColor(Color.parseColor("#D7EEFF"))
             }
+
             4 -> {
                 imageViewBall.setImageResource(R.drawable.basketball_removebg)
                 mainPage.setBackgroundColor(Color.parseColor("#FFD7D7"))
